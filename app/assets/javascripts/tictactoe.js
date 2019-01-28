7c8ba7a1b2bd44b45ed58063df65c986e7e022bd
lafesh <ginafedder@yahoo.com>
2019-01-28 15:49:54 -0700

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -3,7 +3,8 @@ $(document).ready(function() {
     attachListeners()
 })
 
-let turn = 0
+var turn = 0
+var currentGame 
 
 const winningCombinations = [
     [0, 1, 2],
@@ -20,28 +21,57 @@ function attachListeners() {
     $('td').click(doTurn)
 
     $('#save').click(saveGame)
-    $('#previous').click(function(event) {
-        
-    })
-    $('#clear').click(function(event) {
-        
-    })
+    $('#previous').click(previousGames)
+    $('#clear').click(clearGame)
     
 }
 
 function saveGame() {
+    event.preventDefault()
+    var tds = document.querySelectorAll("td")   
+    var data = {state: [tds[0].innerText, tds[1].innerText, tds[2].innerText, tds[3].innerText, tds[4].innerText, tds[5].innerText, tds[6].innerText, tds[7].innerText, tds[8].innerText]}
+    $.ajax({
+        type: currentGame ? 'PATCH' : 'POST',
+        url: currentGame ? '/games/' + currentGame : '/games',
+        data: data, 
+        success: function(response) {
+            currentGame = response.data.attributes.id
+        }
+     })
+}
+
+function previousGames() {
+    event.preventDefault()
+    $.ajax({
+        type: 'GET',
+        url: '/games',
+        success: function(response) {
+            var gamelist = document.getElementById("games")
+            while (gamelist.hasChildNodes()) { gamelist.removeChild(gamelist.firstChild)}
+            response.data.map(a => $('#games').append('<li><a href="#" onclick="getGame(this)">' + a.id + '</a></li>'))
+        }
+    })
+}
+
+function getGame(content) {
+    event.preventDefault()
+    $.ajax({
+        type: 'GET',
+        url: '/games/' + parseInt(content.innerText),
+        data: content.innerText,
+        success: function(response) {
+            setMessage("")
+            currentGame = response.data.id
+            $("td").each(function(e) {$(this).html(response.data.attributes.state[e])})
+        }
+    })
+}
+
+function clearGame() {
+    event.preventDefault()
+    setMessage("")
     var tds = document.querySelectorAll("td")
-    
-    var value = Array.from(tds).map(td => td.innerHTML)
-    
-    if(document.querySelector("table").dataset.id === undefined) {
-        fetch('/games.json', {
-            method: 'POST',
-            body: $(value).serializeArray()
-        })
-        .then(response => response.json())
-        .then(json => console.log("itworked", json))
-    }
+    tds[0].innerText = "", tds[1].innerText = "", tds[2].innerText = "", tds[3].innerText = "", tds[4].innerText = "", tds[5].innerText = "", tds[6].innerText = "", tds[7].innerText = "", tds[8].innerText = ""
 }
 
 function player(){
@@ -62,16 +92,20 @@ function checkWinner() {
     winningCombinations.map(combo => {
         if(tds[combo[0]].innerHTML == tds[combo[1]].innerHTML && tds[combo[0]].innerHTML == tds[combo[2]].innerHTML && tds[combo[0]].innerHTML !== "") {
             setMessage(`Player ${player()} Won!`)
+            return true
         }
     })
 }
 
 function doTurn(event) {
-    if (this.innerHTML === "") {
+    if ( this.innerHTML === "" && $("#message")[0].innerText == "" ) {
         updateState(this)
         checkWinner()
         turn ++
-    } 
+    } else if ($("#message")[0].innerText !== "") {
+        turn = 0
+        debugger
+    }
     
     //setMessage("Tie game.")
     
